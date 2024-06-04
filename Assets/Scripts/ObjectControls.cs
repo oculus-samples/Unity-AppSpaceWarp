9bb162065d9515b3d2d0883f718cdfe3063e0601
Ben Walker <benwa@hey.com>
2024-06-04 14:58:56 -0700

    Update to UPM+v65 integration packages. Split into multiple scenes.

--- Assets/Scripts/ObjectControls.cs
+++ Assets/Scripts/ObjectControls.cs
@@ -1,7 +1,6 @@
-// Copyright (c) Facebook, Inc. and its affiliates.
-// Use of the material below is subject to the terms of the MIT License
-// https://github.com/oculus-samples/Unity-AppSpaceWarp/tree/main/Assets/LICENSE
+// Copyright (c) Meta Platforms, Inc. and affiliates.
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +12,8 @@ public class ObjectControls : MonoBehaviour
     static string ZWRITE_KEYWORD = "_ZWrite";
     static string TEXTURE_KEYWORD = "_BaseMap";
 
+    public OVRInput.RawButton ToggleButton;
+
     [Header("Translation")]
     public bool AllowTranslationChange;
     public Vector3 TranslationDirection;
@@ -20,98 +21,84 @@ public class ObjectControls : MonoBehaviour
     public AnimationCurve TranslationCurve;
     public float[] TranslationMultipliers;
 
+    [Header("Rotation")]
+    public bool AllowRotationChange;
+    public Vector3 RotationAxis;
+    [Tooltip("Measured in degrees per second")]
+    public float[] RotationSpeeds;
+
     [Header("Material Controls")]
     public bool AllowToggleRGB;
     public bool AllowToggleZWrite;
     public bool AllowToggleMotionVec;
+    public bool AllowToggleRenderer;
     public bool AllowToggleTexture;
     public Texture2D[] TextureValues;
-
-    [Header("Object Enabled")]
-    public bool AllowToggleObjectEnabled;
-    public GameObject ObjectToEnable;
+    public bool AllowToggleMesh;
+    public Mesh[] MeshValues;
 
     private Vector3 m_startingTranslation;
-    private int m_currTranslationMultIndex = 0;
     private float m_currTranslationTime = 0;
-
-    private int m_currTextureIndex = 0;
+    private int m_toggleCount = 0;
     private MeshRenderer m_assocMeshRenderer;
-
-    public float CurrentTranslationMultiplier { get { return TranslationMultipliers[m_currTranslationMultIndex]; } }
-    public Texture2D CurrentTexture { get { return TextureValues[m_currTextureIndex]; } }
-    public bool RGBEnabled { get { return m_assocMeshRenderer.material.IsKeywordEnabled(RGB_ENABLED_KEYWORD); } }
-    public bool MotionVecEnabled { get { return m_assocMeshRenderer.material.IsKeywordEnabled(MOTIONVECTOR_ENABLED_KEYWORD); } }
-    public bool ZWriteEnabled { get { return m_assocMeshRenderer.material.GetInt(ZWRITE_KEYWORD) > 0f; } }
+    private MeshFilter m_assocMeshFilter;
 
     void Awake()
     {
         m_startingTranslation = this.transform.position;
+
         m_assocMeshRenderer = GetComponent<MeshRenderer>();
+        m_assocMeshFilter = GetComponent<MeshFilter>();
         if (AllowToggleTexture)
-            m_assocMeshRenderer.material.SetTexture(TEXTURE_KEYWORD, CurrentTexture);
+            m_assocMeshRenderer.material.SetTexture(TEXTURE_KEYWORD, TextureValues[0]);
+        if (AllowToggleMesh)
+            m_assocMeshFilter.sharedMesh = MeshValues[0];
     }
 
     void Update()
     {
+        if (OVRInput.GetDown(ToggleButton))
+        {
+            ++m_toggleCount;
+
+            if (AllowToggleRGB)
+            {
+                if (m_assocMeshRenderer.material.IsKeywordEnabled(RGB_ENABLED_KEYWORD))
+                    m_assocMeshRenderer.material.DisableKeyword(RGB_ENABLED_KEYWORD);
+                else
+                    m_assocMeshRenderer.material.EnableKeyword(RGB_ENABLED_KEYWORD);
+            }
+
+            if (AllowToggleZWrite)
+                m_assocMeshRenderer.material.SetInt(ZWRITE_KEYWORD, m_assocMeshRenderer.material.GetInt(ZWRITE_KEYWORD) > 0f ? 0 : 1);
+
+            if (AllowToggleMotionVec)
+            {
+                if (m_assocMeshRenderer.material.IsKeywordEnabled(MOTIONVECTOR_ENABLED_KEYWORD))
+                    m_assocMeshRenderer.material.DisableKeyword(MOTIONVECTOR_ENABLED_KEYWORD);
+                else
+                    m_assocMeshRenderer.material.EnableKeyword(MOTIONVECTOR_ENABLED_KEYWORD);
+            }
+                
+            if (AllowToggleTexture)
+                m_assocMeshRenderer.material.SetTexture(TEXTURE_KEYWORD, TextureValues[m_toggleCount % TextureValues.Length]);
+
+            if (AllowToggleMesh)
+                m_assocMeshFilter.sharedMesh = MeshValues[m_toggleCount % MeshValues.Length];
+
+            if (AllowToggleRenderer)
+                m_assocMeshRenderer.enabled = !m_assocMeshRenderer.enabled;
+        }
+
         if (AllowTranslationChange)
         {
-            m_currTranslationTime += Time.deltaTime * CurrentTranslationMultiplier;
+            m_currTranslationTime += Time.deltaTime * TranslationMultipliers[m_toggleCount % TranslationMultipliers.Length];
             this.transform.position = m_startingTranslation + (TranslationDirection * TranslationCurve.Evaluate(m_currTranslationTime));
         }
-    }
 
-    public void ToggleTranslationMultiplier()
-    {
-        if (AllowTranslationChange)
-            m_currTranslationMultIndex = (m_currTranslationMultIndex + 1) % TranslationMultipliers.Length;
-    }
-
-    public void ToggleRGB()
-    {
-        if (AllowToggleRGB)
+        if (AllowRotationChange)
         {
-            if (RGBEnabled)
-                m_assocMeshRenderer.material.DisableKeyword(RGB_ENABLED_KEYWORD);
-            else
-                m_assocMeshRenderer.material.EnableKeyword(RGB_ENABLED_KEYWORD);
-        }
-    }
-
-    public void ToggleMotionVectors()
-    {
-        if (AllowToggleMotionVec)
-        {
-            if (MotionVecEnabled)
-                m_assocMeshRenderer.material.DisableKeyword(MOTIONVECTOR_ENABLED_KEYWORD);
-            else
-                m_assocMeshRenderer.material.EnableKeyword(MOTIONVECTOR_ENABLED_KEYWORD);
-        }
-    }
-
-    public void ToggleZWrite()
-    {
-        if (AllowToggleZWrite)
-        {
-            m_assocMeshRenderer.material.SetInt(ZWRITE_KEYWORD, ZWriteEnabled ? 0 : 1);
-        }
-    }
-
-    public void ToggleTexture()
-    {
-        if (AllowToggleTexture)
-        {
-            m_currTextureIndex = (m_currTextureIndex + 1) % TextureValues.Length;
-            m_assocMeshRenderer.material.SetTexture(TEXTURE_KEYWORD, CurrentTexture);
-        }
-        
-    }
-    
-    public void ToggleObjectEnabled()
-    {
-        if (AllowToggleObjectEnabled)
-        {
-            ObjectToEnable.SetActive(!ObjectToEnable.activeSelf);
+            this.transform.rotation = this.transform.rotation * Quaternion.AngleAxis(Time.deltaTime * RotationSpeeds[m_toggleCount % RotationSpeeds.Length], RotationAxis);
         }
     }
 }
